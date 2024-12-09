8c28b47180e7b6f233e0f4d1208854b434e52ee3
Akshay Uperia <uperiaakshay1592@gmail.com>
2024-12-08 22:13:50 -0500

    changes in portfolio

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-d94e19056553465ef298.js"
+    "url": "webpack-runtime-ed4cc38092af880431dd.js"
   },
   {
     "url": "framework-fbf2011d6c2b83ba228a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "27f5cbdd2423ca982372db9d55e7e701"
+    "revision": "5b0af4f29835929a365366cc6f5a578e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-9b1997f2483a747a5203.js"
